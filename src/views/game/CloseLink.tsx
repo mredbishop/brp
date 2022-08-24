4cc26dedc07b5c8bebc78d835820b43fa6477c81
Ed <ed@nexdynamic.com>
2022-08-24 08:14:32 +0100

    Moving the close button

--- src/views/game/CloseLink.tsx
+++ src/views/game/CloseLink.tsx
@@ -4,17 +4,17 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const closeLinkStyle: SxProps<Theme> = {
     position: 'relative',
-    left: '-50%;',
+    left: '0',
     display: 'inline',
     marginLeft: '18px',
     a: {
         fontWeight: '600',
         textDecoration: 'none',
         background: '#444',
-        padding: '6px 10PX',
+        padding: '14px 14px 6px',
+        fontSize: '22px',
         borderRadius: '3px',
         color: 'red',
-        width: '22px',
         transition: '275MS',
         '&:hover': {
             background: '#fff'
